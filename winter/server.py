694da29100beeb037b224105c3f23135e1741915
maxpolk <maxpolk@gmail.com>
2014-03-10 21:22:57 -0400

    Migrated from optparse to argparse

--- winter/server.py
+++ winter/server.py
@@ -1,5 +1,5 @@
 '''
-Command-line interface for Winter.
+Server module for Winter.
 
 This file is part of Winter, a wiki-based computing platform.
 Copyright (C) 2012,2014  Max Polk <maxpolk@gmail.com>
@@ -7,16 +7,15 @@ License located at http://www.gnu.org/licenses/agpl-3.0.html
 '''
 
 # Library imports
-from optparse import OptionParser   # read/parse command-line options
+import argparse                     # read/parse command-line options
 import ConfigParser                 # read/write config file
 import os
 
-# Local imports
+# Import the current package to get package vars like winter.software_name
 import winter
-from winter.interwiki import Interwiki
 
 # Module short description
-module_description = "command-line interface"
+module_description = "server module"
 
 # Module long description
 long_description = "{} ({}): {}, {}".format (
@@ -29,48 +28,60 @@ def setupOptions (args):
     Command-line options override config file options.
     '''
     # All options always exist because we use defaults if not present
-    parser = OptionParser (prog=winter.software_name,
-                           version=winter.software_version,
-                           usage="Usage: %prog [options]",
-                           description=long_description)
+    parser = argparse.ArgumentParser (prog=winter.software_name,
+                                      usage="Usage: %prog [options]",
+                                      description=long_description)
+
+    # Version
+    parser.add_argument (
+        "--version",
+        action="version", version=winter.software_version)
 
     # Verbose
-    parser.add_option (
+    parser.add_argument (
         "-v", "--verbose",
-        dest = "verbose", action = "store_true", default = False,
+        action = "store_true", default = False,
         help = "display more information about client operation")
 
-    # Config file
-    parser.add_option (
+    # Config file for various options
+    parser.add_argument (
         "-c", "--config",
-        dest = "config", default = os.path.expanduser ("~/.winterrc"),
+        default = os.path.expanduser ("~/.winterrc"),
         metavar = "FILE",
         help = "configuration file [default: %default]")
 
     # No config, don't read or write config file, higher priority than -c option
-    parser.add_option (
+    parser.add_argument (
         "-n", "--noconfig",
-        dest = "noconfig", action = "store_true", default = False,
+        action = "store_true", default = False,
         help = "do not use a configuration file")
 
-    # Add a directory for local file wiki
-    parser.add_option (
+    # Directory to be used for file cache, overrides config file value
+    parser.add_argument (
         "-d", "--directory",
-        dest = "directory", default = os.path.expanduser ("~/.winter"),
-        help = "add directory for local file wiki")
+        default = os.path.expanduser ("~/.winter"),
+        help = "directory to be used for file cache")
 
-    # Add a MongoDB collection for database server wiki
-    parser.add_option (
-        "-m", "--mongo",
-        dest = "collection", help = "add mongodb collection for database wiki")
+    # Add hostname where to find MongoDB database
+    parser.add_argument (
+        "--dbhost",
+        default = '127.0.0.1',
+        help = "hostname of MongoDB database")
 
-    # Add an URL for web based wiki
-    parser.add_option (
-        "-u", "--url",
-        dest = "url", help = "add url for web wiki")
+    # Add a port number to MongoDB database
+    parser.add_argument (
+        "--dbport",
+        default = 27017,
+        type = int,
+        help = "port number of MongoDB database")
 
-    # Place results into temp variables
-    (options, arguments) = parser.parse_args (args)
+    # Everything else goes into "arguments" option
+    parser.add_argument (
+        "arguments",
+        nargs = argparse.REMAINDER)
+
+    # Place results into options variable
+    options = parser.parse_args (args)
 
     if (options.verbose):
         def show (name, value):
@@ -83,16 +94,16 @@ def setupOptions (args):
         show ("collection", options.collection)
         show ("url", options.url)
         show ("directory", options.directory)
-        if len(arguments) > 1:
+        if len(options.arguments) > 1:
             print "Parsed command-line arguments:"
-            for arg in arguments[1:]:
+            for arg in options.arguments[1:]:
                 show ("argument", arg)
 
-    return (parser.get_default_values(), options, arguments)
+    return options
 
-def setupSystem (default_options, options):
+def setupSystem (options):
     '''
-    Instantiate and return an interwiki system using the default and given options.
+    Instantiate and return an wiki system using the default and given options.
     '''
     # If not ignoring config file, read it
     if (not options.noconfig):
@@ -105,15 +116,13 @@ def setupSystem (default_options, options):
         print "DEBUG: Writing configuration file (not yet)"
         pass
 
-    # Create an interwiki agent
-    interwiki = Interwiki ()
-    return interwiki
+    return "TODO"
 
-def runCommands (interwiki, arguments):
+def runCommands (wiki):
     print "Running commands: (not yet)"
-    for arg in arguments[1:]:
-        print "    arg:", arg
+    # for arg in arguments[1:]:
+    #     print "    arg:", arg
 
-# Running modules as top-level scripts is an antipattern
+# Running modules as top-level scripts is an antipattern, use bin/snow instead
 if __name__ == '__main__':
     pass
