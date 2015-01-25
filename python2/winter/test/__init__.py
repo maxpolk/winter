fccd5eb29d4b37ac01e7c03ed63bd1b0bb51f495
maxpolk <maxpolk@gmail.com>
2015-01-25 17:18:50 -0500

    Removing support for Python 2

--- python2/winter/test/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-This is the test package of Winter.  To run a full batter of tests
-you can use the script in the ../../bin directory.
-
-This file is part of Winter, a wiki-based computing platform.
-Copyright (C) 2012,2013,2014  Max Polk <maxpolk@gmail.com>
-License located at http://www.gnu.org/licenses/agpl-3.0.html
-'''
