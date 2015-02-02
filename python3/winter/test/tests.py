0480ef238876639c8abf0df544bde39936bef551
maxpolk <maxpolk@gmail.com>
2015-02-01 23:44:19 -0500

    New notify module

--- python3/winter/test/tests.py
+++ python3/winter/test/tests.py
@@ -2,17 +2,18 @@
 Script to run tests.
 
 This file is part of Winter, a wiki-based computing platform.
-Copyright (C) 2012,2014  Max Polk <maxpolk@gmail.com>
+Copyright (C) 2012,2014,2015  Max Polk <maxpolk@gmail.com>
 License located at http://www.gnu.org/licenses/agpl-3.0.html
 '''
 
 import unittest
 
-import winter.server
+from winter import initiate, notify
 
 class TestMetadata (unittest.TestCase):
     def test_description (self):
-        assert len (winter.server.module_description) > 0, 'invalid module_description'
+        assert len (initiate.module_description) > 0, 'initiate: invalid module_description'
+        assert len (notify.module_description) > 0, 'nofity: invalid module_description'
 
 if __name__ == '__main__':
     unittest.main ()
