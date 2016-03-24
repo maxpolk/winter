0c99fcea9cca756e1a4763e0e7785f4f8b7cd91e
maxpolk <maxpolk@gmail.com>
2016-03-24 08:34:21 -0400

    Making bin a namespace

---
+++ python3/bin/__init__.py
@@ -0,0 +1,6 @@
+# This file is part of Winter, a wiki-based computing platform.
+# Copyright (C) 2012,2013,2014,2015  Max Polk <maxpolk@gmail.com>
+# License located at http://www.gnu.org/licenses/agpl-3.0.html
+'''
+This is the script package of Winter.
+'''
